061ef020641ab33fcd360a470f0383b273197169
Chris Nguyen <chris.dungnguyen@gmail.com>
2025-07-03 17:49:02 +0700

    feat: init

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -1,48 +1,19 @@
+require "nvchad.mappings"
+
+-- add yours here
+
 local map = vim.keymap.set
 
--- general
-map("n", "<leader>n", "<cmd> enew <cr>", { desc = "general new buffer" })
-map("n", "<tab>", "<cmd> bnex <cr>", { desc = "general next buffer" })
-map("n", "<S-tab>", "<cmd> bprevious <cr>", { desc = "general prev buffer" })
-map("n", "<C-s>", "<cmd> w <cr>", { desc = "general save buffer" })
+map("n", ";", ":", { desc = "CMD enter command mode" })
+map("i", "jk", "<ESC>")
+
 map("n", "<C-a>", "ggVG", { desc = "general select all" })
-map("n", "D", vim.diagnostic.open_float, { desc = "general show diagnostic error messages" })
-map("n", "<Esc>", "<cmd>noh<cr>", { desc = "general clear highligh" })
-
-map("n", "<C-h>", "<C-w>h", { desc = "switch window left" })
-map("n", "<C-l>", "<C-w>l", { desc = "switch window right" })
-map("n", "<C-j>", "<C-w>j", { desc = "switch window down" })
-map("n", "<C-k>", "<C-w>k", { desc = "switch window up" })
-
-map("i", "<C-h>", "<Left>", { desc = "move left" })
-map("i", "<C-l>", "<Right>", { desc = "move right" })
-map("i", "<C-j>", "<Down>", { desc = "move down" })
-map("i", "<C-k>", "<Up>", { desc = "move up" })
-
 map({ "n", "v" }, "<PageUp>", "^", { desc = "move to beginning of line" })
 map("i", "<PageUp>", "<ESC>^", { desc = "move to beginning of line" })
+map({ "n", "v" }, "<PageUp>", "^", { desc = "move to beginning of line" })
 
--- conform
-map("n", "<leader>b", function()
-	if vim.g.disable_autoformat then
-		vim.cmd("FormatEnable")
-	else
-		vim.cmd("FormatDisable")
-	end
-end, { desc = "general toggle format on save" })
-
--- bdelete
-map("n", "<leader>x", "<cmd> bd <cr>", { desc = "bdelete close buffer" })
-
--- comment
-map("n", "<leader>/", "gcc", { desc = "comment or uncomment", remap = true })
-map("v", "<leader>/", "gc", { desc = "comment or uncomment", remap = true })
-
--- neotree
-map({ "n", "v" }, "\\", ":Neotree toggle<CR>", { desc = "NeoTree toggle", silent = true })
-
--- gitsigns
-map("n", "<leader>gb", "<cmd> Gitsigns blame_line <cr>", { desc = "gitsigns blame line" })
+-- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+map("n", "\\", "<cmd>NvimTreeFocus<CR>")
 
 -- telescope
 map("n", "<leader><space>", "<cmd> Telescope buffers <cr>", { desc = "telescope find buffers" })
@@ -53,8 +24,3 @@ map("n", "<leader>sg", "<cmd> Telescope live_grep <cr>", { desc = "telescope liv
 map("n", "<leader>ss", "<cmd> Trouble diagnostics <cr>", { desc = "trouble diagnostics loclist" })
 map("n", "<leader>sd", "<cmd> Trouble todo <cr>", { desc = "trouble todo" })
 
--- whichkey
-map("n", "<leader>wK", "<cmd> WhichKey <cr>", { desc = "whichkey all keymaps" })
-map("n", "<leader>wk", function()
-	vim.cmd("Whichkey " .. vim.fn.input("WhichKey: "))
-end, { desc = "whichkey query lookup" })
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,233 +1,18 @@
 return {
-	"nvim-lua/plenary.nvim",
-	"nvim-tree/nvim-web-devicons",
-	"famiu/bufdelete.nvim",
-	"tpope/vim-sleuth", -- Detect tabstop and shiftwidth automaticaslly
+  {
+    "stevearc/conform.nvim",
+    event = 'BufWritePre', -- uncomment for format on save
+    opts = require "configs.conform",
+  },
 
-	{
-		"goolord/alpha-nvim",
-		opts = function()
-			local dashboard = require("alpha.themes.dashboard")
-			dashboard.section.buttons.val = {
-				dashboard.button("e", "  New file", "<cmd>ene <CR>"),
-				dashboard.button("SPC s f", "󰈞  Find file"),
-				dashboard.button("SPC s g", "󰊄  Live grep"),
-				dashboard.button("u", "  Update plugins", "<cmd>Lazy sync<CR>"),
-				dashboard.button("q", "󰅚  Quit", "<cmd>qa<CR>"),
-			}
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require "configs.lspconfig"
+    end,
+  },
 
-			return dashboard
-		end,
-		config = function(_, opts)
-			require("alpha").setup(opts.config)
-		end,
-	},
-
-	{
-		"sainnhe/gruvbox-material",
-		priority = 1000,
-		config = function()
-			vim.g.gruvbox_material_better_performance = 1
-			vim.g.gruvbox_material_transparent_background = 2
-			vim.g.gruvbox_material_background = "soft"
-			vim.cmd.colorscheme("gruvbox-material")
-			vim.api.nvim_set_hl(0, "FloatBorder", { bg = "" })
-			vim.api.nvim_set_hl(0, "NormalFloat", { bg = "" })
-		end,
-	},
-
-	{
-		"lukas-reineke/indent-blankline.nvim",
-		event = "VimEnter",
-		main = "ibl",
-		opts = {
-			indent = { char = "┊" },
-			scope = { highlight = { "Normal" } },
-		},
-	},
-
-	{
-		"akinsho/bufferline.nvim",
-		version = "*",
-		dependencies = "nvim-tree/nvim-web-devicons",
-		opts = {},
-	},
-
-	{
-		"nvim-neo-tree/neo-tree.nvim",
-		branch = "v3.x",
-		dependencies = { "nvim-lua/plenary.nvim", "nvim-tree/nvim-web-devicons", "MunifTanjim/nui.nvim" },
-		opts = require("configs.neotree"),
-	},
-
-	{
-		"nvim-lualine/lualine.nvim",
-		enabled = true,
-		event = "VimEnter",
-		dependencies = { "nvim-tree/nvim-web-devicons" },
-		config = function()
-			require("configs.lualine")
-		end,
-	},
-
-	{ -- Useful plugin to show you pending keybinds.
-		"folke/which-key.nvim",
-		opts = {},
-	},
-
-	{ -- formatting!
-		"stevearc/conform.nvim",
-		event = "BufWritePre",
-		opts = require("configs.conform"),
-	},
-
-	{ -- git stuff
-		"lewis6991/gitsigns.nvim",
-		opts = require("configs.gitsigns"),
-	},
-
-	-- lsp stuff
-	{
-		-- `lazydev` configures Lua LSP for your Neovim config, runtime and plugins
-		-- used for completion, annotations and signatures of Neovim apis
-		"folke/lazydev.nvim",
-		ft = "lua",
-		opts = {
-			library = {
-				-- Load luvit types when the `vim.uv` word is found
-				{ path = "${3rd}/luv/library", words = { "vim%.uv" } },
-			},
-		},
-	},
-
-	{
-		"neovim/nvim-lspconfig",
-		dependencies = {
-			{ "mason-org/mason.nvim", opts = {} },
-			"williamboman/mason-lspconfig.nvim",
-			"WhoIsSethDaniel/mason-tool-installer.nvim",
-			"hrsh7th/cmp-nvim-lsp",
-		},
-		config = function()
-			require("configs.lsp").defaults()
-		end,
-	},
-
-	{ -- load luasnips + cmp related in insert mode only
-		"hrsh7th/nvim-cmp",
-		event = "InsertEnter",
-		dependencies = {
-			{
-				-- snippet plugin
-				"L3MON4D3/LuaSnip",
-				dependencies = "rafamadriz/friendly-snippets",
-				opts = { history = true, updateevents = "TextChanged,TextChangedI" },
-				config = function(_, opts)
-					require("luasnip").config.set_config(opts)
-					require("configs.luasnip")
-				end,
-			},
-
-			-- autopairing of (){}[] etc
-			{
-				"windwp/nvim-autopairs",
-				opts = {
-					fast_wrap = {},
-					disable_filetype = { "TelescopePrompt", "vim" },
-				},
-				config = function(_, opts)
-					require("nvim-autopairs").setup(opts)
-
-					-- setup cmp for autopairs
-					local cmp_autopairs = require("nvim-autopairs.completion.cmp")
-					require("cmp").event:on("confirm_done", cmp_autopairs.on_confirm_done())
-				end,
-			},
-
-			-- cmp sources plugins
-			{
-				"saadparwaiz1/cmp_luasnip",
-				"hrsh7th/cmp-nvim-lua",
-				"hrsh7th/cmp-nvim-lsp",
-				"hrsh7th/cmp-buffer",
-				"hrsh7th/cmp-path",
-			},
-		},
-		opts = function()
-			return require("configs.cmp")
-		end,
-	},
-
-	{
-		"nvim-treesitter/nvim-treesitter",
-		build = ":TSUpdate",
-		main = "nvim-treesitter.configs",
-		opts = function()
-			return require("configs.treesitter")
-		end,
-	},
-
-	{
-		"nvim-telescope/telescope.nvim",
-		dependencies = {
-			{
-				"nvim-telescope/telescope-fzf-native.nvim",
-				build = "make",
-				cond = function()
-					return vim.fn.executable("make") == 1
-				end,
-			},
-			"nvim-treesitter/nvim-treesitter",
-			"nvim-telescope/telescope-ui-select.nvim",
-		},
-		cmd = "Telescope",
-		config = function()
-			return require("configs.telescope")
-		end,
-	},
-
-	{
-		"kevinhwang91/nvim-ufo",
-		event = "VeryLazy",
-		dependencies = {
-			"kevinhwang91/promise-async",
-		},
-		config = function()
-			require("configs.ufo")
-		end,
-	},
-
-	{ -- Linting
-		"mfussenegger/nvim-lint",
-		event = { "BufReadPre", "BufNewFile" },
-		config = function()
-			local lint = require("lint")
-			lint.linters_by_ft = {
-				javascript = { "eslint_d" },
-				typescript = { "eslint_d" },
-				javascriptreact = { "eslint_d" },
-				typescriptreact = { "eslint_d" },
-				svelte = { "eslint_d" },
-			}
-
-			-- Create autocommand which carries out the actual linting
-			-- on the specified events.
-			local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
-			vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
-				group = lint_augroup,
-				callback = function()
-					-- Only run the linter in buffers that you can modify in order to
-					-- avoid superfluous noise, notably within the handy LSP pop-ups that
-					-- describe the hovered symbol using Markdown.
-					if vim.opt_local.modifiable:get() then
-						lint.try_lint()
-					end
-				end,
-			})
-		end,
-	},
-
-	{
+  {
 		"folke/todo-comments.nvim",
 		opts = { signs = false },
 	},
@@ -240,109 +25,17 @@ return {
 		},
 	},
 
-	{
-		"luckasRanarison/tailwind-tools.nvim",
-		name = "tailwind-tools",
-		build = ":UpdateRemotePlugins",
-		dependencies = {
-			"nvim-treesitter/nvim-treesitter",
-			"nvim-telescope/telescope.nvim",
-			"neovim/nvim-lspconfig",
-		},
-		opts = {},
-	},
 
-	{
-		"MeanderingProgrammer/render-markdown.nvim",
-		opts = {
-			file_types = { "markdown" },
-		},
-		ft = { "markdown" },
-	},
+  -- test new blink
+  -- { import = "nvchad.blink.lazyspec" },
 
-	{
-		"iamcco/markdown-preview.nvim",
-		cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-		build = "cd app && yarn install",
-		init = function()
-			vim.g.mkdp_filetypes = { "markdown" }
-		end,
-		ft = { "markdown" },
-	},
-
-	{ "echasnovski/mini.diff", version = "*" },
-
-	{
-		"j-hui/fidget.nvim",
-		opts = {
-			notification = {
-				override_vim_notify = true,
-				window = {
-					border = "rounded",
-					winblend = 0,
-				},
-			},
-			progress = {
-				ignore = { "null-ls" },
-			},
-		},
-	},
-
-	-- { "rest-nvim/rest.nvim" },
-
-	-- IMPORTANT: need to install this `brew install libpq`
-	-- {
-	-- 	"kristijanhusak/vim-dadbod-ui",
-	-- 	dependencies = {
-	-- 		{ "tpope/vim-dadbod", lazy = true },
-	-- 		{ "kristijanhusak/vim-dadbod-completion", ft = { "sql", "mysql", "plsql" }, lazy = true }, -- Optional
-	-- 	},
-	-- 	cmd = {
-	-- 		"DBUI",
-	-- 		"DBUIToggle",
-	-- 		"DBUIAddConnection",
-	-- 		"DBUIFindBuffer",
-	-- 	},
-	-- 	keys = {
-	-- 		{ "<leader>e", ":DBUIToggle <CR>", desc = "dbui toggle", silent = true },
-	-- 	},
-	--
-	-- 	init = function()
-	-- 		vim.g.db_ui_use_nerd_fonts = 1
-	-- 	end,
-	-- },
-
-	{
-		"nvimtools/none-ls.nvim",
-		dependencies = { "davidmh/cspell.nvim" },
-		opts = function(_, opts)
-			local cspell = require("cspell")
-			opts.sources = opts.sources or {}
-			table.insert(
-				opts.sources,
-				cspell.diagnostics.with({
-					diagnostics_postprocess = function(diagnostic)
-						diagnostic.severity = vim.diagnostic.severity.WARN
-					end,
-				})
-			)
-			table.insert(opts.sources, cspell.code_actions)
-		end,
-	},
-
-	{
-		"nvzone/floaterm",
-		dependencies = "nvzone/volt",
-		opts = {
-			mappings = {
-				term = function(buf)
-					vim.keymap.set("t", "<C-/>", "<C-\\><C-n>", { silent = true })
-				end,
-			},
-		},
-		cmd = "FloatermToggle",
-		keys = {
-			{ "<C-/>", "<cmd> FloatermToggle <cr>", desc = "Toggle Float Term" },
-		},
-	},
+  -- {
+  -- 	"nvim-treesitter/nvim-treesitter",
+  -- 	opts = {
+  -- 		ensure_installed = {
+  -- 			"vim", "lua", "vimdoc",
+  --      "html", "css"
+  -- 		},
+  -- 	},
+  -- },
 }
